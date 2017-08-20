42282b030aeb23f9647463b59b2148fa7c49f2b5
apolosatov <mailon@list.ru>
2017-08-20 17:54:58 +0300

    add dual

---
+++ eth-ethdcrminer64-dual-my/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal 0xc52cc06ba2451f8d82dddca82e3a8b1064a3e175.mailon/apolosatov@gmail.com -epsw x -dcoin sia -dpool stratum+tcp://sia-eu1.nanopool.org:7777 -dwal cdf45127311c7dc8abf40d997d6e0135072a6c9cbeefc172f3191a6dec37d151942f96acb5ac/mailon/apolosatov@gmail.com -dpsw x -ftime 10
+
